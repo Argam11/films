6bbfb8950beae6abab4899a686cf15e70487fc84
Argam <argam.markosyan1994@gmail.com>
2023-07-20 03:02:14 +0400

    fix pagination color

--- src/Pages/pagination.js
+++ src/Pages/pagination.js
@@ -1,25 +1,23 @@
-import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import { useThemeContext } from "../Contexts/ThemeContext";
 
 export default function PaginationControlled({ page, handleChange }) {
-  // const [page, setPage] = useState(1);
-  // const handleChange = (event, value) => {
-  //   setPage(value);
-  // };
+  const { theme } = useThemeContext();
+  const isDark = theme === "dark";
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '45px' }}>
       <Stack spacing={2}>
-          <Typography style={{ color: 'white' }}>Page: {page}</Typography>
+          <Typography style={{ color: isDark ? 'white' : '#000' }}>Page: {page}</Typography>
         <Pagination
           count={5}
           page={page}
           onChange={handleChange}
-          style={{ color: 'white' }}
+          style={{ color: isDark ? 'white' : '#000' }}
           sx={{
-            '& .MuiPaginationItem-root': { color: 'white', fontSize: '1.2rem' },
+            '& .MuiPaginationItem-root': { color: isDark ? 'white' : '#000', fontSize: '1.2rem' },
             '& .MuiPaginationItem-page.Mui-selected': { backgroundColor: 'rgb(0, 66, 66)', color: 'white' },
           }}
         />
